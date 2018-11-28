de6f6af91dfffb13425eb895d47ca58a66b5d210
Andrzej Korwin-Mikke <andrzej.kmikke@gmail.com>
2018-11-28 22:13:59 +0100

    Moved core functionality to WaylandCore

--- src/WaylandScanner.jl
+++ src/WaylandScanner.jl
@@ -1,59 +1,12 @@
 module WaylandScanner
 import Base.show
 using Base: finalizer
-using FixedPointNumbers
+using WaylandCore
 using LightXML
 
 export wlparse
 
-# Wayland base
-# Native Wayland types
-# Opaque types
-abstract type WlFD end
-abstract type WlObject end
-abstract type WlProxy <: WlObject end
-
-# TEMP aliases for non-opaque, but not yet needed types:
-abstract type WlMessage <: WlObject end
-
-# Primitives:
-const WlInt = Int32
-const WlUInt = UInt32
-"""
-	WlID
-
-Corresponds to "object" and "new_id" protocol types.
-"""
-const WlID = WlUInt # Object ID
-"""
-    WlFixed
-
-24.8 signed fixed-point number based on Int32
-1 sign bit, 23 bits integer prexision, 8 bits decimal precision
-"""
-const WlFixed = Fixed{Int32,8}
-
-# Transparent types:
-struct WlArray{T}
-	size::WlInt # size of content in bytes
-	content::Vector{T} # the content including padding to 32-bit boundary
-end
-struct WlString
-	length::WlInt
-	content::Vector{Cchar} # the content of the string including NUL terminator and padding to 32-bit boundary
-end
-struct WlInterface <: WlObject
-	name::Cstring # Name of the interface
-	version::Int # SInterface version
-	method_count::Int # Number of methods (requests)
-	methods::Ptr{WlMessage} # The array of method signatures
-	event_count::Int # Number of events
-	events::Ptr{WlMessage} # The array of event signatures
-end
-
-# Utility types:
-const WlVersion = Int
-const WlMsgType = Union{WlInt,WlUInt,WlFixed,WlString,WlID,WlArray,WlFD} # message argument types
+# Utility
 """
     typewrap(u)
 
